f93f7a5a08f46c37677040e6cb9d36f6846fa5ee
18234407948 <1332265305@qq.com>
2022-06-26 07:57:39 +0800

    阿里巴巴cloud的nacos 单机使用
    测试分支的

--- SpringbootCloudAlibaba/nacosLearinging02/src/main/java/Capgemini/NacosApplicaiton.java
+++ SpringbootCloudAlibaba/nacosLearinging02/src/main/java/Capgemini/NacosApplicaiton.java
@@ -3,7 +3,7 @@ package Capgemini;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
+//随便写写测试一下
 @SpringBootApplication
 @EnableDiscoveryClient
 public class NacosApplicaiton {
